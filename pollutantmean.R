14248820754b5ea9d4b9a98691b771ada7ffae8f
RorschachSSR <1223465150@qq.com>
2022-02-19 16:17:49 +0800

    make it!!

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,1 +1,35 @@
-#
+pollutantmean<-function(directory,pollutant,id=1:332){
+    old.dir<-getwd()
+    new<-directory
+    new.dir<-paste(old.dir,new,sep="/")
+    setwd(new.dir)
+    len<-length(id)
+    s<-0
+    num<-0
+    name<-function(x){
+        if(x<10)
+            return(paste("00",x,".csv",sep=""))
+        else if(x<100)
+            return(paste("0",x,".csv",sep=""))
+        else
+            return(paste(x,".csv",sep=""))
+    }
+    if(pollutant=="sulfate"){
+      for(i in 1:len){
+          t<-read.csv(name(id[i]))
+          s<-s+sum(t$sulfate,na.rm=TRUE)
+          x<-as.numeric(!is.na(t$sulfate))
+          num<-num+sum(x)
+      }
+    }
+    else{
+        for(i in 1:len){
+            t<-read.csv(name(id[i]))
+            s<-s+sum(t$nitrate,na.rm=TRUE)
+            x<-as.numeric(!is.na(t$nitrate))
+            num<-num+sum(x)
+        }
+    }
+    setwd(old.dir)
+    s/num
+}
